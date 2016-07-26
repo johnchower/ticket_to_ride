cf0dcd0ac458e0368593648ba41c02b895e0bc7c
johnchower <johnchower@gmail.com>
2016-07-26 16:00:14 -0700

    Include betweenness score for tracks

--- ms_basic_centrality_scores.R
+++ ms_basic_centrality_scores.R
@@ -1,8 +1,10 @@
 # Master script: determine basic centrality scores for tracks and cities
 library(reshape2)
+library(plyr)
 library(dplyr)
 library(magrittr)
 library(igraph)
+library(plotly)
 
 source('fn_convert_df_graph_to_igraph.r')
 
@@ -10,6 +12,7 @@ long_city_adjacency_matrix <-
   read.csv("long_city_adjacency_matrix.csv", stringsAsFactors = F)
 
 city_graph <- long_city_adjacency_matrix %>% 
+  select(city_1, city_2) %>%
   convert_df_graph_to_igraph(directed = F)
 
 
@@ -17,6 +20,27 @@ city_graph <- long_city_adjacency_matrix %>%
 city_betweenness <- city_graph %>% 
   betweenness %>% 
   {data.frame(city = names(.), betweenness.score = .)} %>% 
+  mutate(betweenness.score = round(10*betweenness.score/max(betweenness.score),1)) %>%
   arrange(desc(betweenness.score))
 
-# 
+track_betweenness <- city_graph %>% 
+  {
+    betweenness.score = edge_betweenness(.)
+    vertex_names <- city_graph %>% 
+      E %>% 
+      attributes %>% 
+      {.$vnames} %>% 
+      strsplit("|", fixed = T) %>%
+      ldply(
+        .fun = function(v){
+          data.frame(head = v[1], tail = v[2])
+        }
+      ) %>%
+      cbind(betweenness.score) %>%
+      return
+  } %>%
+  unique %>%
+  mutate(betweenness.score = round(10*betweenness.score/max(betweenness.score),1)) %>%
+  arrange(desc(betweenness.score))
+
+# Eigenvector centrality
